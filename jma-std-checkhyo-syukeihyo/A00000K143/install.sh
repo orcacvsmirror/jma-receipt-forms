ef29db98ef5def8e6075e5d57df0081eb8536a2a
take <take@cvs.orca.med.or.jp>
2009-05-27 15:09:22 +0900

    install.shでINIファイルのコピー処理を削除(ver4.4.0対応) プログラムオプション対応 履歴修正

--- jma-std-checkhyo-syukeihyo/A00000K143/install.sh
+++ jma-std-checkhyo-syukeihyo/A00000K143/install.sh
@@ -36,13 +36,6 @@ echomsg "公開帳票 プログラムコピー中..."
 cp -af *.CBL ${SITESRCDIR}/cobol 2> /dev/null
 cp -af *.INC ${SITESRCDIR}/cobol/copy 2> /dev/null
 cp -af *.red ${SITESRCDIR}/form 2> /dev/null
-cp -af version_* ${SITESRCDIR}/doc
-
-# copy *.INI
-FILEINI=`find -name "*.INI" -print 2>/dev/null`
-if test "$FILEINI" != "" ; then
-  cp -ai *.INI ${SITESRCDIR}/data
-fi
 
 # run site-upgrade.sh
 if [ -f ${SCRIPTSDIR}/allways/site-upgrade.sh ] ; then
