fd958dfdb1ca776432b0a3d720909304918c91dd
monma <monma@cvs.orca.med.or.jp>
2012-11-19 11:12:31 +0900

    履歴更新 README.TXTを修正 インストール時のメッセージを変更

--- jma-std-checkhyo-syukeihyo/A00000K140/install.sh
+++ jma-std-checkhyo-syukeihyo/A00000K140/install.sh
@@ -31,7 +31,7 @@ if [ $usrname != "root" ] ; then
 fi
 
 # start message
-echomsg "公開帳票 プログラムコピー中..."
+echomsg "公開帳票 診療行為区分別集計表 診療科別 プログラムコピー中..."
 
 # copy
 cp -af *.CBL ${SITESRCDIR}/cobol 2> /dev/null
@@ -41,13 +41,13 @@ cp -af version_* ${SITESRCDIR}/doc
 
 # run site-upgrade.sh
 if [ -f ${SCRIPTSDIR}/allways/site-upgrade.sh ] ; then
-  sh ${SCRIPTSDIR}/allways/site-upgrade.sh
+  bash ${SCRIPTSDIR}/allways/site-upgrade.sh
 else
-  echomsg "公開帳票 プログラムコピー異常終了!!"
+  echomsg "公開帳票 診療行為区分別集計表 診療科別 プログラムコピー異常終了!!"
   exit 1
 fi
 
 # end message
 sync
-echomsg "公開帳票 プログラムコピー終了!!"
+echomsg "公開帳票 診療行為区分別集計表 診療科別 プログラムコピー終了!!"
 exit 0
